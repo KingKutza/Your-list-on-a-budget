e29602f154e94e82b4aa9ba89ea593a50824b6f8
King Kutza <kingkutzathefirst@gmail.com>
2017-02-27 10:03:24 -0500

    added Table Design Document

---
+++ Table.java
@@ -0,0 +1,54 @@
+
+public class Table {
+	enum Type{text,number,bool};//used to indicate the type of data in a column
+	boolean database; //used to tell that table if it is being used in the database or not
+	String Name;//the name of a table used to identify it
+	int Size;//the number of rows in the table
+	static int CONST_collumn_count=10;//a constant to hold the maximum number of columns that a table can have;
+	static int CONST_OuterDatum_Size=10;
+	static int CONST_InnerDatum_Size=10;
+	Column[] table = new Column[CONST_collumn_count];
+	public void Table(String name,boolean DB){//the default constructor
+		 Name=name;
+		 database=DB;
+		 Size=0;
+	}
+	public void addColumn(Type type,int lenght){
+		
+	}
+	private class Column{// a wrapper class that holds all the data for a collumn as well as the relivant metadata
+		String Name;
+		
+		private class OuterDatum{//a container that holds ten inner datums or 100 data points 
+			int index;
+			InnerDatum[] dataarr = new InnerDatum[CONST_OuterDatum_Size];
+			OuterDatum next = new OuterDatum();//a recursive link to the next outer datum
+			public void OuterDatum(){
+				
+			}
+			private class InnerDatum{// a container for ten data points
+				int index;
+				Data[] dataarr = new Data[CONST_InnerDatum_Size];
+				public void InnerDatum(){
+					
+				}
+				private class Data{// a container for a single data point
+					
+					
+					public void Data(){
+						
+					}
+					private class Text{
+						
+					}
+					private class Num{
+						
+					}
+					private class Bool{
+						
+					}
+				}
+			}	
+		}
+	}
+}
