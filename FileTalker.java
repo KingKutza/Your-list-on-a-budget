892cd832262df581d7739e88db7d1afa708cf0c5
KingKutza <kingkutzathefirst@gmail.com>
2017-04-17 08:44:02 -0400

    Update FileTalker.java

--- FileTalker.java
+++ FileTalker.java
@@ -1,4 +1,6 @@
 package orgin.v02;
+import android.util.Log;
+
 import java.io.*;
 import java.util.Scanner;
 /**
@@ -12,24 +14,27 @@ public class FileTalker {
     int FileLineCount = 0;
     String Text[];
     String Name = "";
+    File Directory;
     static  char FieldDelim ='=';
-    public void FileTalker(){
+    public FileTalker(){
         //does nothing
     }
-    public boolean CreateFile(String name){
+    public boolean CreateFile(File directory,String name){
         Boolean Result = false;
-        File file = new File(name);
-        if(FileExist(name)){
-            //opens the file and then dose nothing else
+        try{
+            File file = new File(directory,name);
+            FileOutputStream a = new FileOutputStream(file);
+            a.close();
             Result = true;
-        }else {
-            Result = false;//indicates and passes back the the opperation failed
+        }catch(Exception E){
+            Result = false;
         }
+        Result = FileExist(directory,name);
         return Result;
     }
-    public boolean DeleteFile(String name){
-        //dose nothing
-        return false;//indicates and passes back the the opperation failed
+    public void DeleteFile(File directory,String name){
+        File temp = new File(directory,name);
+        temp.delete();
     }
     public boolean WrightFile(String[] text){
        boolean Result = false;
@@ -40,20 +45,20 @@ public class FileTalker {
         }
         return Result;
     }
-    public boolean OpenFile(String name){
+    public boolean OpenFile(File directory,String name){
       boolean Result = false;
         if(FileLoaded){CloseFile();}//closes the currently oppen file before attmpting to open a new one
         try{
-           if(FileExist(name)){
-               file = new Scanner(new File(name));
-               outputConcat = new BufferedWriter(new FileWriter(name,true));
-               outputDestruct = new BufferedWriter(new FileWriter(name,false));
-               Name = name;
-               FileLoaded = true;
-               GetLineCount();
-               Text = getText();
-               Result = true;//indicates that the file has been sucsessfully loaded
-           }
+            File temp = new File(directory,name);
+            file = new Scanner(temp);
+            outputConcat = new BufferedWriter(new FileWriter(temp,true));
+            outputDestruct = new BufferedWriter(new FileWriter(temp,false));
+            Name = name;
+            Directory=directory;
+            FileLoaded = true;
+            FileLineCount = GetLineCount();
+            Text = getText();
+            Result = true;//indicates that the file has been sucsessfully loaded
         }catch(Exception e){
             Result = false;//indicates and passes back the the opperation failed
         }
@@ -75,6 +80,7 @@ public class FileTalker {
             outputDestruct = null;
             FileLoaded = false;
             FileLineCount = 0;
+            Directory=null;
             Text = null;
             Result = true;
         }
@@ -87,45 +93,56 @@ public class FileTalker {
                Result += Text[i];
            }
        }else{//in the event that there is not a file loaded returns a error
-           Result = "Null Data Error: There is no File loaded Right now";
+           Log.d("V0.2Debug","File Talker GetField is Returning a null value line:96");
+           Result = null;
        }
         return Result;
     }
-    public String GetField(String name){//assumes that the file is formated as fieldname=fielddata
+    public String GetField(String name){//assumes that the file is formated as fieldname=fielddata;
         String Result="";
         if(FileLoaded){
             for(int i=0;i<Text.length;i++){
-                String FieldName = Text[i].substring(0,Text[i].indexOf(FieldDelim)).trim().toLowerCase();
-                if(FieldName.equals(name.trim().toLowerCase())){
-                    String FieldData = Text[i].substring(Text[i].indexOf(FieldDelim+1)).trim().toLowerCase();
-                    Result = FieldData;
-                    break;//ends the loop the data has been found
-                }else{
-                    continue;//moves onto the next line;
+                if(Text[i]!=null){
+                    String FieldName = Text[i].substring(0,Text[i].indexOf(FieldDelim)).trim().toLowerCase();
+
+                    if(FieldName.equals(name.trim().toLowerCase())){
+                        String FieldData = Text[i].substring(Text[i].indexOf(FieldDelim+1)).trim().toLowerCase();
+                        Result = FieldData;
+                        break;//ends the loop the data has been found
+                    }else{
+                        continue;//moves onto the next line;
+                    }
                 }
+
             }
             if(Result.equals("")){//in the event that the method dose not find anything
-                Result = "Null Data Error: the Field that you are looking for does not exist or is null";
+                Log.d("V0.2Debug","File Talker GetField is Returning a null value line:116");
+                Result = null;
             }
         }else{
-            Result = "Null Data Error: There is not currently a file loaded";
+            Log.d("V0.2Debug","File Talker GetField is Returning a null value line:120");
+            Result = null;
         }
         return Result;
     }
     public Boolean WrightField(String name, String data){
+        Log.d("V0.2Debug","Break Point Four: the value of data is => "+data);
         boolean Result = false;
-        if(FileLoaded){
+        if(FileLoaded&&name!=null&&data!=null){
             name = name.trim().toLowerCase();
             data = data.trim().toLowerCase();
             int line = FileFieldExist(name);
             String Field = name+FieldDelim+data;
+            Log.d("V0.2Debug","Break Point Five: the value of Field is => "+Field);
             if(line>=0){
+                Log.d("V0.2Debug","Break Point Six");
                 Replace(Field,line);
                 WrightReload();
                 Result = true;
             }else if(line==-1){
+                Log.d("V0.2Debug","Break Point Seven");
                 WrightLine(Field);
-                Reload();
+               // Reload();
                 Result = true;
             }else{
                Result = false;
@@ -133,25 +150,35 @@ public class FileTalker {
         }
         return Result;
     }
-    public Boolean FileExist(String name){
+    public Boolean FileExist(File directory,String name){
+        boolean Result = false;
         try{
-            FileInputStream temp = new FileInputStream(name);
-            temp.close();
-            return true;
+            File temp = new File(directory,name);
+            Result = temp.exists();
         }catch(Exception e){
-            return false;
+            Result = false;
         }
+        return Result;
     }
     private String[] getText(){
+        Log.d("V0.2Debug","~~~");
         String Result[];
         if(FileLoaded){
-             Result = new String[FileLineCount];
-            for(int i=0;i<Result.length;i++){
+            Log.d("V0.2Debug","~A~");
+            if(FileLineCount<=10){
+                Log.d("V0.2Debug","~B~");
+                Result = new String[10];
+            }else{
+                Log.d("V0.2Debug","~C~");
+                Result = new String[FileLineCount];
+            }
+            for(int i=0;i<FileLineCount;i++){
                 Result[i]=file.nextLine();
+                Log.d("V0.2Debug","~D~"+Result[i]);
             }
         }else{
-            Result = new String[1];
-            Result[0] = "Null Data Error: There is not currently a file loaded";
+            Log.d("V0.2Debug","~E~");
+            Result = new String[10];
         }
 
         return Result;
@@ -166,16 +193,20 @@ public class FileTalker {
         return Result;
     }
     private int FileFieldExist(String name){
+
         int Result = -2;//-2 idicates that the file is not loaded
         if(FileLoaded){
             for(int i=0;i<Text.length;i++){
-                String FieldName = Text[i].substring(0,Text[i].indexOf(FieldDelim)).trim().toLowerCase();
-                if(FieldName.equals(name.trim().toLowerCase())){
-                    Result = i;
-                    break;//ends the loop the data has been found
-                }else{
-                    continue;//moves onto the next line;
+                if(Text[i]!=null){
+                    String FieldName = Text[i].substring(0,Text[i].indexOf(FieldDelim)).trim().toLowerCase();
+                    if(FieldName.equals(name.trim().toLowerCase())){
+                        Result = i;
+                        break;//ends the loop the data has been found
+                    }else{
+                        continue;//moves onto the next line;
+                    }
                 }
+
             }
             if(Result==-2){//in the event that the method dose not find anything
                 Result = -1;//indicates that the field dose not exist
@@ -186,16 +217,21 @@ public class FileTalker {
     private boolean WrightLine(String text){
         boolean Result = false;
         if(FileLoaded){
+
             try{
+                Log.d("V0.2Debug","Break Point Eight");
                 outputConcat.write(text);
+                Log.d("V0.2Debug","Break Point Nine");
                 Result = true;
             }catch(Exception e){
+                Log.d("V0.2Debug","Break Point Ten");
                Result = false;
             }
         }
         return Result;
     }
     private boolean Replace(String text,int line){
+
         boolean Result = false;
         if(FileLoaded){
             Text[line] = text;
@@ -204,6 +240,7 @@ public class FileTalker {
         return Result;
     }
     private boolean WrightReload(){
+        Log.d("V0.2Debug","Debug Point Five");
         boolean Result = false;
         if(FileLoaded){
             if(Wright()){
@@ -217,9 +254,10 @@ public class FileTalker {
     private boolean Reload(){
         boolean Result = false;
         if(FileLoaded){
-          String temp = Name;
+            String name = Name;
+            File directory = Directory;
             if(CloseFile()){
-                if(OpenFile(temp)){
+                if(OpenFile(directory,name)){
                     Result = true;
                 }
             }
@@ -243,6 +281,4 @@ public class FileTalker {
         }
         return Result;
     }
-
-
 }
