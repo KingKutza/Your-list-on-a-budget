f1e46c27d250351a3574c37fa55b18537db8efc1
KingKutza <kingkutzathefirst@gmail.com>
2017-04-17 08:46:58 -0400

    Create Home.java

---
+++ Home.java
@@ -0,0 +1,74 @@
+package orgin.v02;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.R.*;
+import java.io.*;
+import java.io.FileOutputStream;
+
+public class Home extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        runtime();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void runtime(){//runs on the creation of the page
+        Log.d("V0.2Debug","Congradulations the 'Your list on a Budget App' is up and runing");
+        Log.d("V0.2Debug","The Home Page has opened");
+
+
+
+
+    }
+    public void SettingsBtnClick(View v){
+        startActivity(new Intent(getApplicationContext(),Setting_page.class));
+    }
+    public void ListBtnClick(View v){
+        Log.d("V0.2Debug","The on click listener works for the List Button");
+    }
+    public void SavedBtnClick(View v){
+        Log.d("V0.2Debug","The on click listener works for the Saved Button");
+    }
+
+}
